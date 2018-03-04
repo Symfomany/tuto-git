4e963e47019848ebf63eebf9b27fd6881bd54b0a
Symfomany <zuzu38080@gmail.com>
2018-03-04 20:45:10 +0100

    add Footer

---
+++ js/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer>
+                    <ul>
+                        <li>Ok</li>
+                    </ul>
+                </footer>
+            </div>
+        )
+    }
+}
