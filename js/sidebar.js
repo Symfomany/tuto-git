f6909db6e65babb4c8acb723bd2b7bdf54c263e5
Julien Boyer <zuzu38080@gmail.com>
2018-03-04 10:41:47 +0100

    add sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+    render() {
+        return (
+            <div>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
